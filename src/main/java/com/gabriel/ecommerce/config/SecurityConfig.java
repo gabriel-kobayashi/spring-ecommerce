833b4b71dacd39d9bc2087b66fa9a6ecaab4c911
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-20 18:04:53 -0300

    feat: libera rota pública de cadastro de usuário

--- src/main/java/com/gabriel/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/gabriel/ecommerce/config/SecurityConfig.java
@@ -2,11 +2,34 @@ package com.gabriel.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/gabriel/ecommerce/controller/AuthController.java
+++ src/main/java/com/gabriel/ecommerce/controller/AuthController.java
@@ -5,7 +5,6 @@ import com.gabriel.ecommerce.dto.UserResponseDto;
 import com.gabriel.ecommerce.entity.User;
 import com.gabriel.ecommerce.service.UserService;
 import jakarta.validation.Valid;
-import jdk.jshell.Snippet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,13 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
+@RequestMapping("/auth")
+public class AuthController {
 
     private UserService service;
 
-    public UserController(UserService service) {
+    public AuthController(UserService service) {
         this.service = service;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDto> create(@RequestBody @Valid UserRegisterDto dto) {
+        User user = service.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UserResponseDto(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getRole().name()));
+    }
 }
