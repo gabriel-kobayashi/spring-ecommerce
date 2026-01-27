7b1747b79bd76a22e0880573e9b0410877e9d19a
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-27 10:13:08 -0300

    refactor: renomear AddtoCartRequestDto para AddToCartRequestDto

--- src/main/java/com/gabriel/ecommerce/dto/cart/AddtoCartRequestDto.java
+++ src/main/java/com/gabriel/ecommerce/dto/cart/AddtoCartRequestDto.java
@@ -1,8 +1,14 @@
 package com.gabriel.ecommerce.dto.cart;
 
-public record AddtoCartRequestDto(
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
+public record AddToCartRequestDto(
+
+        @NotNull(message="O productId é obrigatório")
         Long productId,
+
+        @Min(value=1, message="A quantidade deve ser maior que zero")
         int quantity
 ) {
 }
