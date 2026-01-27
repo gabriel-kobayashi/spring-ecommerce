aa1ef873c7e5eb650d8555fe081055db59c1d62c
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-27 14:16:18 -0300

    feat(order): criar entidades iniciais de pedido

---
+++ src/main/java/com/gabriel/ecommerce/entity/Order.java
@@ -0,0 +1,32 @@
+package com.gabriel.ecommerce.entity;
+
+import com.gabriel.ecommerce.entity.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> items = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    private BigDecimal total;
+
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/gabriel/ecommerce/entity/enums/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.gabriel.ecommerce.entity.enums;
+
+public enum OrderStatus {
+    CREATED,
+    PAID,
+    CANCELED
+}
