0781d18603a2b4aa63db3fe0b4ed71807e525211
Gabriel Kobayashi <gabrielkobayashi11@gmail.com>
2026-01-28 14:07:45 -0300

    feat: cria entidade Payment e enum PaymentStatus

---
+++ src/main/java/com/gabriel/ecommerce/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.gabriel.ecommerce.entity;
+
+import com.gabriel.ecommerce.entity.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "order_id", nullable = false, unique = true)
+    private Order order;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    private LocalDateTime paidAt;
+}
---
+++ src/main/java/com/gabriel/ecommerce/entity/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.gabriel.ecommerce.entity.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PAID,
+    FAILED
+}
